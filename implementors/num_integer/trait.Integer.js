ac801350a355092d2c60bf98a04e577090216555
Travis CI User <travis@example.org>
2016-12-05 21:38:45 +0000

    Documentation for diesel-rs/diesel@49e3cdc

---
+++ implementors/num_integer/trait.Integer.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors["num_integer"] = [];implementors["chrono"] = [];implementors["diesel"] = [];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
---
+++ implementors/rand/distributions/trait.Sample.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors["regex_syntax"] = [];implementors["libc"] = [];implementors["rand"] = [];implementors["num"] = [];implementors["chrono"] = [];implementors["quickcheck"] = [];implementors["quickcheck"] = [];implementors["diesel"] = [];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
